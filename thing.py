1cd1572e01917665d73d62e0484f01d94daa980a
JonSnap <jonconneryl@gmail.com>
2018-11-04 04:14:54 +0000

    Going to sleep. I'll finish shit in the morning.

--- thing.py
+++ thing.py
@@ -3,25 +3,26 @@ import os
 
 eyePath = 'imgDir\eyeImg'
 hairPath = 'imgDir\hairImg'
-
-with open('test.txt', 'r') as file:
-	fileVar = file.readlines()
-	fileVar = [i.strip() for i in fileVar]
-sepList = [i.split('\t') for i in fileVar]
-imgList = []
-for i in sepList:
-	strang = ''
-	RSID = i[0]
-	SNP = i[3]
-	if i[1] == '0.1':
-		strang += 'M'
-	if i[1] == '0.2':
-		strang += 'F'
-	if i[2] == '0.1':
-		os.chdir(eyePath)
-		strang += 'E'
-		if 
-	elif i[2] == '0.3':
-		os.chdir(hairPath)
-		strang += 'HH'
-	
+bodyPath = 'imgDir\\bodyImg'
+''''
+def imgGet(listSuper):
+	sepList = [i.split('\t') for i in fileVar]
+	imgList = []
+	for i in sepList:
+		strang = ''
+		RSID = i[0]
+		SNP = i[3]
+		if i[1] == '0.1':
+			strang += 'M'
+		if i[1] == '0.2':
+			strang += 'F'
+		if i[2] == '0.1':
+			os.chdir(eyePath)
+			strang += 'E'
+			if 
+		elif i[2] == '0.3':
+			os.chdir(hairPath)
+			strang += 'HH'
+'''
+os.chdir(bodyPath)
+print(os.getcwd())
